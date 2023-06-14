4364722a15281c3ff1150882f5d644f38b605958
channaveer <channaveerpatil999@gmail.com>
2023-06-14 22:53:05 +0530

    about page created

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,20 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 import hero from '../../public/hero.png';
+import Button from '@/component/Button/Button';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.item}>
         <h1 className={styles.title}>
-          Bettrer design for your digital products
+          Better design for your digital products
         </h1>
         <p className={styles.desc}>
           Turning your Idea Into Reality , we bring together the teams from the
           global tech industry
         </p>
-        <button className={styles.btn}>See Our Work</button>
+        <Button text='See Our Work' url='/portfolio' />
       </div>
       <div className={styles.item}>
         <Image src={hero} alt='hero' className={styles.img} />
