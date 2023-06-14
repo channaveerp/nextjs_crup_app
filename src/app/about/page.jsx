36862302374c8a72c701cb180ab75f295dfec52d
channaveer <channaveerpatil999@gmail.com>
2023-06-14 08:52:02 +0530

    working on about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import styles from './page.module.css';
+import Image from 'next/image';
 
 const About = () => {
-  return <div className={styles.container}>About</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src='https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/dream-about-the-ocean-mathieu-rivrin.jpg'
+          fill={true}
+          alt='about'
+          className={styles.img}
+        />
+        <h1></h1>
+      </div>
+      <div className={styles.textContainer}></div>
+    </div>
+  );
 };
 
 export default About;
